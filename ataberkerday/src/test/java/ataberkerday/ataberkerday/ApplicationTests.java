2f386db083c2221ee4557bae94396d8c6f9999b0
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-08-10 18:17:51 +0300

    update

---
+++ ataberkerday/src/test/java/ataberkerday/ataberkerday/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ataberkerday.ataberkerday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
